7987637e6a7acd9634bd715f598c5b9f5fb9d021
Unknown <Heshams12@hotmail.com>
2018-03-28 10:59:35 -0700

    Made changes to project.

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // require("./routes/api_routes.js")(app);
-require("./routes/html_routes.js")(app);
+require("./routes/html-routes.js")(app);
 
 db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
