52c7229de009a64137c0ef1a752c5a892ea6521f
Unknown <Heshams12@hotmail.com>
2018-03-29 10:23:02 -0700

    fixed errors

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const methodOverride = require("method-override");
-app.use(methodOverride("_method"));
-
 const db = require("./models");
 
 app.use(bodyParser.urlencoded({ extended: true }));
