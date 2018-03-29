fb3e425794d28d4f553dffdfef2aad0d1aa45438
Nick <nickkwon92@gmail.com>
2018-03-29 10:03:05 -0700

    Testing out some scripts

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,21 @@
 // TEST SECTION FOR API REQUESTS - REMOVE ONCE FUNCTIONAL
 
-$.get("/api/puppies", function(data) {
+$.get("/api/puppies", function (data) {
     $("#test").html(data);
-      }); 
+});
 
-$("#shelSubmit").on("click", function(){
-    var newData = 
-    {
-        name: $("#name").val().trim(),
-        breed: $("#breed").val().trim(),
-        age: $("#age").val().trim(),
-        gender: $("#gender").val().trim(),
-        image: $("#imgURL").val().trim(),
-        info: $("#info").val().trim()
-    };
+$("#shelSubmit").on("click", function () {
+    var newData =
+        {
+            name: $("#name").val().trim(),
+            breed: $("#breed").val().trim(),
+            age: $("#age").val().trim(),
+            gender: $("#gender").val().trim(),
+            image: $("#imgURL").val().trim(),
+            info: $("#info").val().trim()
+        };
     var currentURL = window.location.origin;
-	$.post(currentURL + "/api/puppies", newData,
-					function (data) { });
+    $.ajax({ url: currentURL + "/api/puppies", method: "POST" }).done(function (res) {
+        console.log(res)
+    });
 })
